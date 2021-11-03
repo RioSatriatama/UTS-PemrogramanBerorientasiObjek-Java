6619a418cdf666caf0257eb5f397a847580ea78f
RioSatriatama <82066488+RioSatriatama@users.noreply.github.com>
2021-11-03 11:49:02 +0700

    Add files via upload

---
+++ uts_pbo/Handphone.java
@@ -0,0 +1,28 @@
+package uts_pbo;
+
+public class Handphone { //ini merupakan bagian main class
+	private String Type;
+    private String OS;
+    private Integer RAM;
+
+    public String getType() {
+        return Type;
+    }
+    public void setType(String type) {
+        Type = type;
+    }
+    public String getOS() {
+        return OS;
+    }
+    public void setOS(String oS) {
+        OS = oS;
+    }
+    public Integer getRAM() {
+        return RAM;
+    }
+    public void setRAM(Integer rAM) {
+        RAM = rAM;
+    }
+    
+    
+}
---
+++ uts_pbo/Samsung.java
@@ -0,0 +1,22 @@
+package uts_pbo;
+
+public class Samsung extends Handphone {
+
+		private String Merek;
+	    private Double Price;
+
+	    public String getMerek() {
+	        return Merek;
+	    }
+	    public void setMerek(String merek) {
+	        Merek = merek;
+	    }
+	    public Double getPrice() {
+	        return Price;
+	    }
+	    public void setPrice(Double price) {
+	        Price = price;
+	    }
+
+	    
+	}
